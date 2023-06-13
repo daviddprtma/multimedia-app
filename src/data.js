48fad19e0dfcc8574456703af749d27538cbbaff
David Pratama <davidchristianpratama@gmail.com>
2023-06-13 14:29:41 +0700

    successfully make this multimedia app :)

---
+++ src/data.js
@@ -0,0 +1,41 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/Announcement To Twitch channel.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/Bluesy Vibes (Sting) - Doug Maxwell_Media Right Productions.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/tes.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/chilll.png"
+    },
+    
+    {
+        id: 5,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/christmasgrenade.jpg"
+    },
+    
+    {
+        id: 6,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/my twitch.png"
+      },
+  ]
+  
